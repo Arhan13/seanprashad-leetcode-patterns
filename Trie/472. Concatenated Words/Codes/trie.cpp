bfcd07b56efa92d5059665895cd1bd566c2d3183
Arhan13 <arhanchoudhury@gmail.com>
2023-07-17 02:13:24 +0530

    Added LC 472

--- Trie/472. Concatenated Words/Codes/trie.cpp
+++ Trie/472. Concatenated Words/Codes/trie.cpp
@@ -1,6 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Time Complexity: O(N * L^2)
+// Time Complexity explanation: N is the number of words in the input list, L is the average length of words.
+//                              For each word, we need to traverse the word, so the time complexity is O(N * L).
+//                              For each word, we need to traverse the word again to check if it is a concatenated word,
+//                              so the time complexity is O(N * L^2).
+// Is this true after memoization? Yes, because the memoization is based on the word and the wordCount, so the time complexity
+//                                  is O(N * L^2).
+// Space Complexity: O(N * L)
+// Space Complexity explanation: N is the number of words in the input list, L is the average length of words.
+//                               For each word, we need to traverse the word, so the space complexity is O(N * L).
+
+// Intution: We can use a Trie to store all the words in the input list. Then we can traverse each word in the input list, and
+//           check if it is a concatenated word. We can use DFS to check if it is a concatenated word. We can use memoization
+//           to optimize the time complexity.
+
+// Logic for the DFS:
+// 1. If the currentIndex is greater than or equal to the wordSize, we can return wordCount > 1.
+// 2. We can get the key by concatenating the substring of the word from currentIndex to wordSize and the wordCount.
+// 3. If the key is in the memo, we can return the value of the key.
+// 4. We can traverse the word from currentIndex to wordSize.
+// 5. If the current character is not in the Trie, we can return false.
+// 6. If the current character is in the Trie, we can get the node of the current character.
+// 7. If the current character is the end of a word, we can check if the rest of the word is a concatenated word.
+// 8. If the rest of the word is a concatenated word, we can return true.
+// 9. If the rest of the word is not a concatenated word, we can continue to traverse the word.
+// 10. If the current character is not the end of a word, we can continue to traverse the word.
+// 11. If we cannot find a concatenated word, we can return false.
+// 12. If we can find a concatenated word, we can return true.
+// 13. We can store the key and the value in the memo.
+
+// Give me a nice leetcode solution title -
+//  Trie + DFS + Memoization
+
+#include <bits/stdc++.h>
+using namespace std;
+
 struct TrieNode
 {
     TrieNode *links[26];
@@ -73,6 +109,7 @@ public:
 class Solution
 {
 private:
+    unordered_map<string, bool> memo;
     bool dfs(string word, TrieNode *root, int currentIndex, int wordCount)
     {
         int wordSize = word.size();
@@ -80,23 +117,31 @@ private:
         {
             return wordCount > 1;
         }
+        string key = word.substr(currentIndex, wordSize) + "-" + to_string(wordCount);
+        if (memo.count(key))
+        {
+            return memo[key];
+        }
         TrieNode *node = root;
         for (int i = currentIndex; i < wordSize; i++)
         {
             if (!node->containsKey(word[i]))
             {
+                memo[key] = false;
                 return false;
             }
             node = node->get(word[i]);
             if (node->isEnd())
             {
-                if (dfs(word, node, currentIndex + 1, wordCount + 1))
+                if (dfs(word, root, i + 1, wordCount + 1))
                 {
+                    memo[key] = true;
                     return true;
                 }
             }
         }
-        return false;
+        memo[key] = false;
+        return memo[key];
     }
 
 public:
