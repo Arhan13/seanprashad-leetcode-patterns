6ad3a118ff0e115fe1d51c635529d9e9e68f03b0
Arhan13 <arhanchoudhury@gmail.com>
2023-07-31 04:30:20 +0530

    Added LC 300 Binary Search Approach

---
+++ DP/1D-DP/300. Longest Increasing Subsequence/Codes/binary_search.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int lengthOfLIS(vector<int> &nums)
+    {
+        vector<int> temp;
+        temp.push_back(nums[0]);
+        for (int i = 1; i < nums.size(); i++)
+        {
+            if (nums[i] > temp.back())
+            {
+                temp.push_back(nums[i]);
+            }
+            else
+            {
+                // Find the lower bound for nums[i]
+                int ind =
+                    lower_bound(
+                        temp.begin(),
+                        temp.end(),
+                        nums[i]) -
+                    temp.begin();
+                temp[ind] = nums[i];
+            }
+        }
+        return temp.size();
+    }
+};
