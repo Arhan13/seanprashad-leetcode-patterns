a292d079fcb314c9bc8d076544b869f0dd781800
Arhan13 <arhanchoudhury@gmail.com>
2023-08-17 02:59:19 -0400

    Added LC 24

---
+++ LikedList/24. Swap Nodes in Pairs/Codes/rec.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    ListNode *swapPairs(ListNode *head)
+    {
+        if (!head || !head->next)
+        {
+            return head;
+        }
+        ListNode *firstNode = head;
+        ListNode *secondNode = head->next;
+
+        firstNode->next = swapPairs(secondNode->next);
+        secondNode->next = firstNode;
+
+        return secondNode;
+    }
+};
