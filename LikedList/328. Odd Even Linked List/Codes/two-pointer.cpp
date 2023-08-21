068c052832809a905ac481ec897c882637e96083
Arhan13 <arhanchoudhury@gmail.com>
2023-08-21 19:40:09 -0400

    Added LC 328 to the LinkedList folder

---
+++ LikedList/328. Odd Even Linked List/Codes/two-pointer.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    ListNode *oddEvenList(ListNode *head)
+    {
+        if (!head || !head->next)
+        {
+            return head;
+        }
+        // We will make two lists odd and even
+        ListNode *odd = head;
+        ListNode *even = head->next;
+        ListNode *evenHead = head->next;
+
+        while (odd->next && even->next)
+        {
+            odd->next = even->next;
+            even->next = odd->next->next;
+            odd = odd->next;
+            even = even->next;
+        }
+
+        odd->next = evenHead;
+        return head;
+    }
+};
